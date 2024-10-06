74ac6c957f938ab415074d0d52df9991dd1f718f
Ahmed El-Khayyat <81445232+elkhayyat@users.noreply.github.com>
2024-10-06 22:44:03 +0300

    Version 1.0.5 (#15)
    * Fix: Serving Media Files
    
    * Update: Version 1.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dj-access-logger',
-    version='1.0.4',
+    version='1.0.5',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
