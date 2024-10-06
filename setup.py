8a19a75707ed010e39a8f1a1511fec6aa44e6bde
Ahmed El-Khayyat <81445232+elkhayyat@users.noreply.github.com>
2024-10-06 22:14:11 +0300

    Versions/1.0.4 (#12)
    * Fix: exception when request.body includes file
    
    * Fix: sqlite x django-tenants compatibility issue
    
    * Fix: Update version and build

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dj-access-logger',
-    version='1.0.3',
+    version='1.0.4',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
